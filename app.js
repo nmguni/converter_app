6d951395b3e35f4866fa379d3509b620af3a343d
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-09-18 11:40:14 -0400

    round method added

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+// open close
+var $cell = $(".card");
+
+//open and close card when clicked on card
+$cell.find(".js-expander").click(function() {
+  var $thisCell = $(this).closest(".card");
+
+  if ($thisCell.hasClass("is-collapsed")) {
+    $cell
+      .not($thisCell)
+      .removeClass("is-expanded")
+      .addClass("is-collapsed")
+      .addClass("is-inactive");
+    $thisCell.removeClass("is-collapsed").addClass("is-expanded");
+
+    if ($cell.not($thisCell).hasClass("is-inactive")) {
+      //do nothing
+    } else {
+      $cell.not($thisCell).addClass("is-inactive");
+    }
+  } else {
+    $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+    $cell.not($thisCell).removeClass("is-inactive");
+  }
+});
+
+//close card when click on cross
+$cell.find(".js-collapser").click(function() {
+  var $thisCell = $(this).closest(".card");
+
+  $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+  $cell.not($thisCell).removeClass("is-inactive");
+});
+
 const cards = document.getElementsByClassName("card");
 const expand = document.getElementsByClassName("js-expander");
 const closeCard = document.getElementsByClassName("close_card")[0];
@@ -40,35 +74,35 @@ let grams_id = document.getElementById("grams");
 function calc_grams() {
   massInput.addEventListener("input", e => {
     let grams = e.target.value;
-    pounds.innerHTML = grams * 0.00220462;
-    kilos.innerHTML = grams * 0.001;
-    oz.innerHTML = grams * 0.03527;
+    pounds.innerHTML = (grams * 0.00220462).toFixed(3);
+    kilos.innerHTML = (grams * 0.001).toFixed(3);
+    oz.innerHTML = (grams * 0.03527).toFixed(3);
   });
 }
 // GRAMS calculation FUNCTION
 function calc_kilos() {
   massInput.addEventListener("input", e => {
     let kilos = e.target.value;
-    pounds.innerHTML = kilos * 2.20462;
-    grams_id.innerHTML = kilos * 1000;
-    oz.innerHTML = kilos * 35.274;
+    pounds.innerHTML = (kilos * 2.20462).toFixed(3);
+    grams_id.innerHTML = (kilos * 1000).toFixed(3);
+    oz.innerHTML = (kilos * 35.274).toFixed(3);
   });
 }
 
 function calc_oz() {
   massInput.addEventListener("input", e => {
     let oz = e.target.value;
-    pounds.innerHTML = oz * 0.0625;
-    grams_id.innerHTML = oz * 28.3495;
-    kilos.innerHTML = oz * 0.0283495;
+    pounds.innerHTML = (oz * 0.0625).toFixed(3);
+    grams_id.innerHTML = (oz * 28.3495).toFixed(3);
+    kilos.innerHTML = (oz * 0.0283495).toFixed(3);
   });
 }
 
 function calc_lbs() {
   massInput.addEventListener("input", e => {
     let lbs = e.target.value;
-    kilos.innerHTML = lbs / 0.4535923;
-    grams_id.innerHTML = lbs * 453.592;
+    kilos.innerHTML = (lbs / 0.4535923).toFixed(3);
+    grams_id.innerHTML = (lbs * 453.592).toFixed(3);
     oz.innerHTML = lbs * 16;
   });
 }
@@ -115,37 +149,3 @@ function mass_selection() {
     oz_.classList.remove("display_none");
   }
 }
-
-// open close
-var $cell = $(".card");
-
-//open and close card when clicked on card
-$cell.find(".js-expander").click(function() {
-  var $thisCell = $(this).closest(".card");
-
-  if ($thisCell.hasClass("is-collapsed")) {
-    $cell
-      .not($thisCell)
-      .removeClass("is-expanded")
-      .addClass("is-collapsed")
-      .addClass("is-inactive");
-    $thisCell.removeClass("is-collapsed").addClass("is-expanded");
-
-    if ($cell.not($thisCell).hasClass("is-inactive")) {
-      //do nothing
-    } else {
-      $cell.not($thisCell).addClass("is-inactive");
-    }
-  } else {
-    $thisCell.removeClass("is-expanded").addClass("is-collapsed");
-    $cell.not($thisCell).removeClass("is-inactive");
-  }
-});
-
-//close card when click on cross
-$cell.find(".js-collapser").click(function() {
-  var $thisCell = $(this).closest(".card");
-
-  $thisCell.removeClass("is-expanded").addClass("is-collapsed");
-  $cell.not($thisCell).removeClass("is-inactive");
-});
